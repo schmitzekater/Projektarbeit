a5027adc9a86b44196731fa7304884fbe12c6ce1
schmitzekater <alexander@schmitzekater.de>
2013-11-07 17:23:49 +0100

    Umgestellt auf PHP Uhrzeit und Datum in der Linkseite Links ausgeladen in andere Datei.

--- Projektarbeit_Team_Neu-Ulm/php/links.php
+++ Projektarbeit_Team_Neu-Ulm/php/links.php
@@ -7,4 +7,14 @@
 		<li>Punkt 3</li>
 		<li>Punkt 12</li>
 	</ul>
+	<div id="time">
+	<?php
+	$timestamp = time();
+	$tage = array("Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag","Samstag");
+	$datum = date("d.m.Y",$timestamp);
+	$wochentag = date("w",$timestamp);
+	$uhrzeit = date("H:i",$timestamp);
+	echo $uhrzeit, "<br>",$tage[$wochentag],", ",$datum;
+	?>
+</div>
 </aside>
