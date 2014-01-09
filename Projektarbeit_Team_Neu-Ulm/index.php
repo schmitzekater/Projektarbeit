5db2b73a60a0c436665851440600f1b8f4be9014
Caro <carolindressel@gmx.de>
2014-01-09 17:15:47 +0100

    ujujuj

--- Projektarbeit_Team_Neu-Ulm/index.php
+++ Projektarbeit_Team_Neu-Ulm/index.php
@@ -25,18 +25,7 @@
 			<p>
 				Link: <a href="imprint.html">Impressum </a>
 			</p>
-			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-				nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-				erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-				et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-				Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-				sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-				et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-				accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-				no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-				dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-				tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-				voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+			<p>HIER SOLLTE UNSER TEXT HIN!!!!
 				Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
 				dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in
 				vulputate velit esse molestie consequat, vel illum dolore eu feugiat
