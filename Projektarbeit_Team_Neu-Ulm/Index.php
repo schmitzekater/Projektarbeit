e1b44aeed14ac09c9f80465a8f80901bb84ac0d6
Caro <carolindressel@gmx.de>
2014-03-01 17:49:10 +0100

    css Version1

---
+++ Projektarbeit_Team_Neu-Ulm/Index.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+<title>GenBank</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="./css/main.css" />
+	<link rel="stylesheet" type="text/css" href="./css/nav.css" />
+
+
+</head>
+
+<body>
+<div id="container">
+
+<header>
+<img src ="Bilder/bg_header_start_01.jpg" 
+	header id="headerpicture">
+
+</header>
+
+<nav>
+   
+<body>
+<nav id="nav">
+	<ul id="navigation">
+		<li><a href="#" class="first">Home</a></li>
+		<li><a href="#">Services &raquo;</a>
+			<ul>
+				<li><a href="upload.php">Patentendatenupload</a></li>
+				<li><a href="#">MutDatenbankupload</a></li>
+				<li><a href="#">Ergebnisse speichern &raquo;</a>			
+				
+			</ul>
+		</li>
+		<li><a href="#">Weiterführende Links &raquo;</a>
+			<ul>
+				<li><a href="#">UCSC</a></li>
+				<li><a href="#">NCBI</a></li>
+				<li><a href="#">HGNC</a></li>
+				
+				
+				
+			</ul>				
+		</li>
+		
+		<li><a href="#" class="last">Contact</a></li>
+		<li><a href="#" class="last">Impressum</a></li>
+	</ul>
+</nav>
+
+</body>
+</html>
+
+</nav>
+
+<section id="content">
+		  <article>
+		  <p></p>
+		  <p></p>
+<h1>&nbsp;</h1>
+<div id="main">
+			<h1>Herzlich willkommen beim Mutfinder</h1>
+			<p>Diese Seite bietet ein Tool zur Identifizierung von beschriebenen Mutationen
+			   in der Molekulargenetik.	
+			</p>
+			<p>
+				Link: <a href="imprint.html">Impressum </a>
+			</p>
+			<p>Beschreibung:
+				Um diese Seite nutzen zu können ist es notwendig, sich als Nutzer zu authorisieren.
+				Hierzu nutzen Sie bitte die Eingabemaske an der rechten Seite.
+				Anschließend können Sie unter der Uploadseite eine Liste der gewünschten / gefundenen Mutationen hochladen.
+				Diese Liste wird dann mit einer internen Datenbank verglichen und falls eine Referenz zu den angegebenen Veränderungen
+				gefunden wird, wird diese ausgegeben.</p>
+				
+			<p>Bei Problemen oder Fragen bezüglich dieses Tools wenden Sie sich bitte an den Administrator. Die Angaben finden Sie im Impressum.</p>	
+				
+				
+			<table>
+				<tr>
+					<th>Header 1</th>
+					<th>Header 2</th>
+					<th>Header 3</th>
+					<th>Header 4</th>
+				</tr>
+				<tr>
+					<td>Spalte 1</td>
+					<td>Spalte 2</td>
+					<td>Spalte 3</td>
+					<td>Spalte 2ue0riwjepfjwepjfpew4</td>
+				</tr>
+				<tr>
+					<td rowspan="2" colspan="2">4er Kasten</td>
+					<td colspan="2">2er Kaokgpowrkgopwekgpowekopgkfsten</td>
+				</tr>
+				<tr>
+					<td>Vorletzter</td>
+					<td>Letzter</td>
+				</tr>
+			</table>
+		</div>
+
+  </article>
+  </section>
+
+  <aside>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+<h1>Login</h1>
+			<table border="none">
+				<form name="login" method="get" action="x"
+					onsubmit="popuptest();">
+					<tr>
+						<td>Benutzername</td>
+						<td><input type="text" name="name" length="16"></td>
+					</tr>
+					<tr>
+						<td>Passwort</td>
+						<td><input type="text" name="password" length="16"></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="submit" name="knopf" value="speichern"></td>
+					</tr>
+				</form>
+			</table>
+			<p></p>
+		    <p></p>
+			<h1>Subside</h1>
+			<p>Hier könnte noch erklärender Text zu main stehen. Lorem ipsum
+				dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+				tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+				voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+				Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+				dolor sit amet. Lorem ipsum dolor sit amet, 
+				 </p>
+
+</aside>
+
+<footer>		
+    <h2>&lt;footer&gt;</h2>
+
+
+Copyright
+ </footer>
+</div>
+</body>
+</html>
