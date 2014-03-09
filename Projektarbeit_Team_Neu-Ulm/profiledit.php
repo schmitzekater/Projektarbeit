26a76efc7d082106f9526644c4cd65ec8d9506d0
schmitzekater <alexander@schmitzekater.de>
2014-03-09 22:39:19 +0100

    Fehlende Dateien hinzugefügt.

---
+++ Projektarbeit_Team_Neu-Ulm/profiledit.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+<title>GenBank</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="./css/main.css" />
+	<link rel="stylesheet" type="text/css" href="./css/nav.css" />
+
+
+</head>
+
+<body>
+<?php include "./php/nav.php"; ?>
+
+<section id="content">
+		  <article>
+		  <p></p>
+		  <p></p>
+<h1>&nbsp;</h1>
+<div id="main">
+			<?php
+		require_once('includes/sitzungsstart.php');
+        $seitentitel = 'Profil bearbeiten';
+		require_once('includes/zugang.php');  
+  
+    ?>
+	
+<div id="wrapper">
+	
+	<?php
+	
+		require_once('includes/header.php');  
+		require_once('includes/menu.php');
+				
+		if (!isset($_SESSION['id'])) {
+			echo '<p class="fail">Um auf diese Seite zugreifen zu k&ouml;nnen, m&uuml;ssen Sie sich <a href="login.php">einloggen</a>.</p>';
+			exit();
+		}
+		
+		if (isset($_POST['submit'])) {
+ 
+			$db = mysqli_connect("localhost", "dbuser", "dbuser", "GenBank");
+				mysqli_set_charset($db, "utf8");
+  
+			$passwort = mysqli_real_escape_string($db, trim($_POST['password']));
+			$newpw = mysqli_real_escape_string($db, trim($_POST['newpw']));
+			$wdhnewpw = mysqli_real_escape_string($db, trim($_POST['wdhnewpw']));
+			$nutzername = $_SESSION['nutzername'];
+  
+			if(!empty($passwort) && !empty($newpw) && !empty($wdhnewpw)) { 
+
+				$sql = "SELECT nutzername, passwort FROM nutzer WHERE nutzername = '$nutzername' AND passwort = SHA('$passwort')";
+   
+				$daten = mysqli_query($db, $sql);
+   
+				if (mysqli_num_rows($daten) == 1) {
+					if ($newpw == $wdhnewpw) {
+					
+						mysqli_query($db, "UPDATE nutzer SET passwort = SHA('$newpw') WHERE nutzername = '$nutzername'")
+							or die(mysqli_error());
+							
+						mysqli_close($db);
+		 
+						echo '<p class="pass">Ihr Passwort wurde erfolgreich ge�ndert.</p>';
+		 
+					}
+					else {
+						echo '<p class="fail">Die beiden neuen Passw�rter stimmen nicht �berein.</p>';
+					}
+                }
+				else {
+					echo '<p class="fail">Ihr aktuelles Passwort simmt nicht.</p>';
+				}
+			}
+			else {
+				echo '<p class="fail">Sie haben die ben�tigten Felder nicht ausgef�llt.</p>';
+			}  
+		}  
+	?>
+
+	<form method="post" action="profiledit.php">
+ 
+		<label for="nutzername">Aktuelles Passwort:</label>
+		<input type="password" id="password" name="password"/>
+	  
+		<label for="newpw">Neues Passwort:</label>
+		<input type="password" id="newpw" name="newpw" />
+	  
+		<label for="wdhnewpw">Wiederholung neues Passwort:</label>
+		<input type="password" id="wdhnewpw" name="wdhnewpw" />
+	  
+		<input type="submit" value="Aktivieren" name="submit" />
+	
+	</form>
+  
+	<?php require_once('includes/footer.php'); ?>
+  
+</div> <!-- #wrapper -->
+  </article>
+  </section>
+
+  <aside>
+  
+  
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+			<H2>Information</H2>
+			<p></p>
+		  
+			<p>Um die Funktionen dieser Seite nutzen zu können, ist ein Login notwendig. </p>
+			<p><a href="login.php">Login </a></p>
+			<p>Falls Sie noch nicht registriert sind, können Sie dies unter  <a href="registrieren.php">Registrieren </a> tun.</p>
+			<p>Wir wünschen viel Freude mit dieser Webseite</p>
+  
+
+</aside>
+
+
+<footer>		
+
+<img class="DNABild" src="Bilder/dna-Strang.jpg" width="40" high="50">
+<p>für weitere DNA-Info´s nutzen Sie bitte</p>
+
+<p></p>
+<p>----------------------------------------------------------------------------------------------------------------------------</p>
+<p>fdasdsda</p>
+<p>fdasdsda</p>
+Copyright
+</footer>
+</div>
+</body>
+</html>
+
+
+
---
+++ Projektarbeit_Team_Neu-Ulm/vergessen.php
@@ -0,0 +1,76 @@
+	<?php
+
+		$seitentitel = 'Passwort vergessen';
+		require_once('includes/zugang.php');
+	?>
+	
+<div id="wrapper">
+
+	<?php
+        require_once('includes/header.php');  
+        require_once('includes/menu.php');
+
+		if (isset($_POST['submit'])) {
+			// Mit der Datenbank verbinden
+			$db = mysqli_connect("localhost", "dbuser", "dbuser", "GenBank");
+			mysqli_set_charset($db, "utf8");
+      
+			// Die eingegebenen Login-Daten abrufen
+			$nutzername = mysqli_real_escape_string($db, trim($_POST['nutzername']));
+			$email = mysqli_real_escape_string($db, trim($_POST['email']));
+
+			if (!empty($nutzername) && !empty($email)) {
+				// Benutzername und Passwort in der Tabelle nachschlagen
+				$sql = "SELECT email, nutzername FROM nutzer WHERE nutzername = '$nutzername' AND email = '$email'";
+				$daten = mysqli_query($db, $sql);
+				$zeile = mysqli_fetch_array($daten);
+
+				if (mysqli_num_rows($daten) == 1) {
+				// Abfrage erfolgreich, also Daten an E-Mail senden und Sicherheitscode in die Datenbankspeichern inkl. Code generieren.	  
+					$vergessencode = rand(100000, 999999);
+		
+					mysqli_query($db, "UPDATE nutzer SET vergessen = '$vergessencode' WHERE nutzername = '$nutzername'")
+						or die(mysqli_error());
+		 
+					$absender = 'admin@neispiel.de';
+					$betreff = "Beispiel - Haben Sie Ihr Passwort vergessen?";
+					$msg = "Hallo $nutzername,\n" .
+					"wenn Sie ihr Passwort vergessen haben besuchen Sie bitte http://localhost/zuruecksetzen.php und �ndern Sie ihr vergessenes Passwort in ein neues gew�nschtes Passwort um.\n" .
+					"Dazu ben�tigen Sie nur Ihren Benutzernamen ( $nutzername ) und einen generierten Sicherheitsschl�ssel ( $vergessencode ). \n" .
+					"Falls Sie Ihr Passwort nicht vergessen haben ignorieren Sie diese E-Mail bitte einfach. \n" . 
+					"Mit freundlichen Gr��en\n" .
+					"Beispielseite";
+
+					mail($email, $betreff, $msg, 'From:' . $absender);
+		
+					echo '<p class="pass">Sie haben einen entsprechenden Sicherheitscode an Ihre E-Mail Adresse erhalten. Sie k�nnen <a href="zuruecksetzen.php">hier</a> Ihr Passwort zur�cksetzen.</p>';
+					
+					mysqli_close($db);
+				}
+				else {
+					echo '<p class="fail">E-Mail und Nutzername stimmen nicht �berein.</p>';
+				}
+			}
+			else {
+				echo '<p class="fail">Sie haben vergessen Daten einzugeben.</p>';
+			}
+		}
+	?>
+	
+	<h2>Passwort�nderung anfordern</h2>
+
+	<form method="post" action="vergessen.php">
+
+		<label for="nutzername">Benutzername:</label>
+		<input type="text" id="nutzername" name="nutzername"/>
+	
+		<label for="email">E-Mail:</label>
+		<input type="text" id="email" name="email" />
+	
+		<input type="submit" value="Anfordern" name="submit" />
+	
+	</form>
+  
+	<?php require_once('includes/footer.php'); ?>
+
+</div><!-- #wrapper -->
