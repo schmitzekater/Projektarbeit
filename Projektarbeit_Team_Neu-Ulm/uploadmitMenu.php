e27758ede0bdc6206d0fe9128dddfb8ee48a86a1
Caro <carolindressel@gmx.de>
2014-03-09 13:01:00 +0100

    Hallo

--- Projektarbeit_Team_Neu-Ulm/uploadmitMenu.php
+++ Projektarbeit_Team_Neu-Ulm/uploadmitMenu.php
@@ -9,6 +9,7 @@
 		<link rel="stylesheet" type="text/css" href="./css/menu.css" />
 		<link rel="stylesheet" type="text/css" href="./css/Seite.css" />
 		<link rel="stylesheet" type="text/css" href="./css/nav.css" />
+		<link rel="stylesheet" type="text/css" href="./style.css" />
 		<!-- <link rel="stylesheet" type="text/css" href="./css/style.css" 	media="screen"> -->
 		<script src="./js/jquery.js" type="text/javascript"></script>
 		<script src="./js/upload.js" type="text/javascript"></script>
@@ -46,17 +47,58 @@
 
 <section id="content">
 				<article>
-				
 
+<div id="wrapper">
+
+<?php
+
+	require_once('includes/sitzungsstart.php');
+	$seitentitel = 'Memberarea';
+	require_once('includes/zugang.php');
+		
+	if(isset($_SESSION['nutzername'])) {
+		$user = $_SESSION['nutzername'];
+
+		$db = mysqli_connect("localhost","dbuser","dbuser","GenBank");
+			mysqli_set_charset($db, "utf8");
+
+		$sql = "SELECT aktiviert, nutzername FROM nutzer WHERE nutzername = '$user' ";
+		$daten = mysqli_query($db, $sql);
+		$zeile = mysqli_fetch_array($daten);
+
+		if ($zeile['aktiviert'] == 0) {
+
+			$aktivierungsseite = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/aktivierung.php';
+				header('Location:' . $aktivierungsseite);
+
+			mysqli_close($db);
+
+		}		
+	}
+?>
+
+<div id="wrapper">
+
+<?php
+	require_once('includes/header.php');
+	require_once('includes/menu.php');
+?>
+
+	<?php
+		if (!isset($_SESSION['id'])) {
+			echo '<p class="fail">Um auf diese Seite zugreifen, m&uuml;ssen Sie sich <a href="login.php">einloggen</a>.</p>';
+			exit();
+		}
+	?>
 
 <p class="info">Sie k&ouml;nnen dies hier sehen, weil sie eingeloggt sind.</p>
 
-<p>
-						Laden Sie bitte die Dateien hoch. Akzeptiert werden ....
-					</p>
-					<p>
-						Bei fehlerhaften Dateien werden Sie benachrichtigt.
-					</p>
+<?php require_once('includes/footer.php'); ?>
+
+</div><!-- #wrapper -->
+<p>Laden Sie bitte die Dateien hoch. Akzeptiert werden ....</p>
+
+<p>Bei fehlerhaften Dateien werden Sie benachrichtigt.</p>
 
 					<div id="main">
 						<p>
