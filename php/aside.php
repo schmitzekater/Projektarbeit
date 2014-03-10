dc454f3f7c97d9796c12d33f88cc0fe710869341
Caro <carolindressel@gmx.de>
2014-03-10 21:32:49 +0100

    ztzt

--- php/aside.php
+++ php/aside.php
@@ -9,7 +9,7 @@
 			<p></p>
 		  
 			<p>Um die Funktionen dieser Seite nutzen zu k��nnen, ist ein Login notwendig. </p>
-			<p><a href="./Admin/login.php">Login </a></p>
-			<p>Falls Sie noch nicht registriert sind, k��nnen Sie dies unter  <a href="./Admin/registrieren.php">Registrieren </a> tun.</p>
+			<p><a href="login.php">Login </a></p>
+			<p>Falls Sie noch nicht registriert sind, k��nnen Sie dies unter  <a href="registrieren.php">Registrieren </a> tun.</p>
 			<p>Wir w��nschen viel Freude mit dieser Webseite</p>
   
