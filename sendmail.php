8e3db80deaef7b68aa882e7413ee5580127b1a42
Caro <carolindressel@gmx.de>
2014-03-14 22:20:04 +0100

    gel?scht....

--- sendmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sendmail = "From: sender@yourdomain.com\r\n";
-mail(
- 'carolinschwerdtfeger@gmx.net', // recipient email address
- 'Hallo', // email subject
- 'Toll', // email body
-$sendmail . "\r\n"// additional headers
-);
-?>
